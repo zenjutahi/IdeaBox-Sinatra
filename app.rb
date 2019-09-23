43fd71e77c80038d9cc471f14f25ee1b55a4145c
Zenjutahi <zenjutahi@gmail.com>
2019-09-23 10:24:31 +0300

    [chore] - creating route classes

--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
   configure :development do
     register Sinatra::Reloader 
   end
   
+  not_found do
+    erb :error
+  end
+  
+  post '/' do
+    # 1. Create an idea based on the from parameters
+    idea = Idea.new
+    # 2. Store is
+    idea.save
+    # 3. Send user back to index page to see Ideas 
+    "Creating an IDEA!"
+  end
+
   get '/' do
     erb :index 
   end
