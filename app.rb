ebbe425c8db0c0d86b90e79a4994ac18e44ca315
Zenjutahi <zenjutahi@gmail.com>
2019-09-23 18:06:06 +0300

    [chore] - impliment edit ides

--- app.rb
+++ app.rb
@@ -15,6 +15,20 @@ class IdeaBoxApp < Sinatra::Base
     erb :index, locals: {ideas: Idea.all}
   end
   
+  put '/:id' do |id|
+    data = {
+      :title => params['idea_title'],
+      :description => params['idea_description']
+    }
+    Idea.update(id.to_i, data)
+    redirect '/'
+  end
+  
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+  
   delete '/:id' do |id|
     Idea.delete(id.to_i)
     redirect '/'
--- idea.rb
+++ idea.rb
@@ -14,9 +14,26 @@ class Idea
     end
   end
   
+  def self.update(id, data)
+    database.transaction do
+      database['ideas'][id] = data
+    end
+  end
+  
   def self.raw_ideas
     database.transaction do |db|
       db['ideas'] || []
+    end
+  end
+  
+  def self.find(id)
+    raw_idea = find_raw_idea(id)
+    Idea.new(raw_idea[:title], raw_idea[:description])
+  end
+  
+  def self.find_raw_idea(id)
+    database.transaction do
+      database['ideas'].at(id)
     end
   end
   
