d6cfe963fa17c84844e6cf6be07287502c37b4e3
Zenjutahi <zenjutahi@gmail.com>
2019-09-24 16:06:25 +0300

    [chore] - rank ideas

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -4,17 +4,22 @@ class Idea
   def initialize(attributes)
     @title = attributes["title"]
     @description = attributes["description"]
+    @rank = attributes["rank"] || 0
   end
   
-  # def database
-  #   Idea.database
-  # end
-  # 
-  # def save
-  #   database.transaction do |db|
-  #     db['ideas'] ||= []
-  #     db['ideas'] << {"title" => title, "description" => description}
-  #   end
-  # end
-  # 
+  def save
+    IdeaStore.create(to_h)
+  end
+  
+  def to_h
+    {
+      "title" => title,
+      "description" => description,
+      "rank" => @rank
+    }
+  end
+  
+  def like!
+    @rank += 1
+  end
 end
